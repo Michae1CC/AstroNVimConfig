143fc44f1f06a11a006c60aee1c319ca1c8d8a95
Michael CC <333mmcc@gmail.com>
2023-08-19 18:55:59 +1000

    Updated treesitter opts

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,7 +3,22 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+      "vimdoc", "javascript", "typescript", "c", "lua", "rust", "python"
     })
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    opts.sync_install = false
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    opts.auto_install = true
+    opts.highlight = {
+      -- `false` will disable the whole extension
+      enable = true,
+
+      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+      -- Using this option may slow down your editor, and you may see some duplicate highlights.
+      -- Instead of true it can also be a list of languages
+      additional_vim_regex_highlighting = false,
+    }
   end,
 }
